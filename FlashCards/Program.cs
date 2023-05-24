1f798eace2ebda03f215d4512a6cc5960fcea737
Kho Van Kiryl <whiteboywithnotattoos@gmail.com>
2023-05-24 13:23:51 +0300

    Start application as docker compose

--- FlashCards/Program.cs
+++ FlashCards/Program.cs
@@ -22,14 +22,8 @@ builder.Services.AddScoped<ICardListRepository, EFCardListRepository>();
 var app = builder.Build();
 
 
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapControllers();
 DbInitializer.Initialize(app);
